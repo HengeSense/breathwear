124e41f96ec347c9397f855dd551bd076c459461
Cassidy Saenz <cassidy.saenz@gmail.com>
2012-12-04 16:01:56 -0800

    Added the CorePlot framework to project and linked library, dealt with nit-picky linking/compling details.  Creating empty ViewController for our visualization - BrythmViewController

---
+++ Brythm/BrythmViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrythmViewController.h
+//  Brythm
+//
+//  Created by Cassidy Robert Coyote Saenz on 12/4/12.
+//  Copyright (c) 2012 Cassidy Robert Coyote Saenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrythmViewController : UIViewController
+
+@end
---
+++ Brythm/BrythmViewController.m
@@ -0,0 +1,39 @@
+//
+//  BrythmViewController.m
+//  Brythm
+//
+//  Created by Cassidy Robert Coyote Saenz on 12/4/12.
+//  Copyright (c) 2012 Cassidy Robert Coyote Saenz. All rights reserved.
+//
+
+#import "BrythmViewController.h"
+#import "CorePlot-CocoaTouch.h"
+
+@interface BrythmViewController ()
+
+@end
+
+@implementation BrythmViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
